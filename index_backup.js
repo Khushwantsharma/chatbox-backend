dbac357292e78ae15b66099104e86381ec2d2344
Khushwant Sharma <500062206@stu.upes.ac.in>
2021-04-21 17:30:29 +0530

    minor changes

--- index_backup.js
+++ index_backup.js
@@ -1,13 +1,11 @@
-const app=require('express')();
-const server=require('http').createServer(app);
-const options={
-    cors:true,
-    origins:["http://127.0.0.1:3000","*"]
-}
-
-const io=require('socket.io')(server,options);
-const users=[];
-
+const app=require("express")();
+const server=require("http").createServer(app);
+const io=require("socket.io")(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        method:['get','post'],
+    },
+});
 app.use((req,res,next)=>{
     // res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Origin','*');
@@ -15,100 +13,102 @@ app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization,Authentication');
     next();
 })
-const userexist=(username)=>{
-    if(users.length>1){
-        for (let na of users){
-            if(na.username==username){
-                return false;
-            }
-        }
-    }
-    return true;
-}
-const statusoffline=(username)=>{
-    console.log("status called");
-        for (let i=0;i<users.length;i++){
-            if(users[i].username==username){         
-                users[i].online=false;
-                break; 
-            }
-        }
-}
-const statusonline=(username)=>{
-    console.log("status called");
-        for (let i=0;i<users.length;i++){
-            if(users[i].username==username){         
-                if(users[i].online){
-                    return false;
+app.get('/', function(req, res) {
+    res.send('<h1>h<h1>');
+ });
+ const users=[];
+ let flag;
+const userexist=(username,id)=>{
+    flag=0;
+    if(users.length>0){
+        for(let i=0;i<users.length;i++){
+            if(users[i].name==username){
+                if(!users[i].online){
+                    users[i].online=true;
+                    users[i].id=id;
+                    flag=1;
+                    return false;  
                 }
-                users[i].online=true;
-                break; 
+                return true
             }
+            
         }
-        return true;
+    }
+    users.push({name:username,id:id,online:true});
+    return false
 }
-app.get('/',(req,res,next)=>{
-    console.log('Hit');
-    res.send('<h1>Hello</h1>');
-});
-io.use((socket,next)=>{
-    const username=socket.handshake.auth.username;
-    if(userexist(username)){
-        users.push({username:username,online:true});
-        socket.broadcast.emit("new-user", {
-            name: username
-        });
-    }
-    else{
-        if(statusonline(username)){
-            socket.broadcast.emit('status-online',username);
-        }
-        else{
-            //if a user login from one multiple location then
-            // this will track the number of windows
-            if(socket.dup){
-                socket.dup=socket.dup+1;
-            }
-            else{
-                socket.dup=1;
-            }
+const online=()=>{
+    for(let i=0;i<users.length;i++){
+        if(users[i].name==username){   
+            return true;
         }
     }
+    return false;
+}
+const offline=(username)=>{
+    console.log("[offline]",username);
+    for(let i=0;i<users.length;i++){
+        if(users[i].name==username){
+            users[i].online=false;
+            users[i].id=null;
+            break;
+        }
+    }
+}
+const recevier=(dat)=>{
+    let k=0;
+    for(let i=0;i<users.length;i++){
+        if(users[i].name==dat){    
+            k=users[i].id;
+            break;
+        }
+    }
+    return k;
+}
+
+ io.use((socket,next)=>{
+    const username = socket.handshake.auth.username;
+    console.log(username,socket.id);
+    if(userexist(username,socket.id,)){
+        //code to disconnect user.
+        console.log("user already present");
+        const err = new Error("not authorized");
+        err.data = { content: "Please retry later" };
+        return next(err);
+    }
+    
     socket.username=username;
-    console.log("username:",socket.username,"users:",users);
-
-    //if(!users.find(username)){
-        //users.push(username);
-    //}
     next();
-})
-io.on('connection',(socket)=>{
-    console.log("Hey connection established");
-    socket.emit("users",users,socket.username);
-    socket.on('mes',me=>{
-        console.log(me);
-        socket.broadcast.emit('mes',me);
-    });
-    socket.onAny((event, ...args) => {
-        console.log("From onAny",event, args);
-      });
-    socket.on('disconnect',()=>{
-        // console.log(users.find(socket.username));
-        if(socket.dup){
-            socket.dup=socket.dup-1;
-        }else{
-            let a=statusoffline(socket.username);
-            socket.broadcast.emit('status-offline',socket.username);
-        }
-
-        // users[a].online=false;
-        //socket.broadcast.emit('mes',users);
-        console.log('disconnected Successfully');
-    });
-
-});
-
-
-server.listen(3001,()=>{
-    console.log("Listening on port 3001");
-});
+ })
+ io.on('connection',(socket)=>{
+    if(flag==1){
+        socket.broadcast.emit('online',socket.username);
+    }else{
+    socket.broadcast.emit('new-user',users[users.length-1]);
+    }
+    let f; 
+    if(users.length==1){
+        f=[];
+     }else{
+        f=users.filter(dat=>{return dat.name!=socket.username});
+     }
+    // console.log("userconnected socket",users,'f:',f);
+    socket.emit('users',f);
+    // setTimeout(function() {
+    //     socket.emit('mes','Sent a message 4seconds after connection!');
+    //  }, 4000);
+     socket.on('private-message',(content,sender)=>{
+        console.log("client:",content,sender);
+        let to=recevier(sender);
+        socket.to(to).emit('private-message',content,socket.username);
+    })
+     socket.on('disconnect',()=>{
+        socket.broadcast.emit('offline',socket.username);
+        offline(socket.username);
+        console.log("users offlinelist:",users);
+        console.log("user disconnected");
+    })
+ });
+ server.listen(3001,()=>{
+     console.log("listeing on 3001");
+ });
